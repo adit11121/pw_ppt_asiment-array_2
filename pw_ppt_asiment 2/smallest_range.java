9b51cbc2e43854eb987036c7f71c0136188c21ed
adit11121 <128092002+adit11121@users.noreply.github.com>
2023-06-26 23:21:39 +0530

    Add files via upload

---
+++ pw_ppt_asiment 2/smallest_range.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class smallest_range {
+    
+    public int smallestRangeI(int[] A, int k) {
+        Arrays.sort(A);
+        if(A[0]+k >= A[A.length-1]-k)
+            return 0;
+        return A[A.length-1]-k-(A[0]+k);
+    }
+}
